ec2f26b1f6724725163af8c8536b241fc2e89713
Suemura <39115013+Suemura@users.noreply.github.com>
2019-09-03 23:59:20 +0900

    add alpha setting

--- export_gif.py
+++ export_gif.py
@@ -22,14 +22,26 @@ class GIF_OT_ExportOperator(bpy.types.Operator):
         output_path = context.scene.render.filepath
         image_path_list = glob.glob(output_path+"/*.png", recursive=True)
         image_list = []
-        for img_path in image_path_list:
-            print(img_path)
-            im = Image.open(img_path)
-            alpha = im.split()[3]
-            im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
-            mask = Image.eval(alpha, lambda a: 255 if a <=128 else 0)
-            im.paste(255, mask)
-            image_list.append(im)
+
+        # "use_alpha"
+        if context.scene["gif_use_alpha"] == True:
+            for img_path in image_path_list:
+                print(img_path)
+                im = Image.open(img_path)
+                alpha = im.split()[3]
+                im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
+                mask = Image.eval(alpha, lambda a: 255 if a <=128 else 0)
+                im.paste(255, mask)
+                image_list.append(im)
+        else:
+            for img_path in image_path_list:
+                print(img_path)
+                im = Image.open(img_path)
+                image_list.append(im)
+
+        # "invert"
+        if context.scene["gif_invert"] == True: image_list.reverse()
+
         return image_list[0], image_list[1:]
 
 
@@ -38,14 +50,24 @@ class GIF_OT_ExportOperator(bpy.types.Operator):
         output_path = context.scene.render.filepath
         first_img, image_list = self.load_image_file(context)
 
-        first_img.save(
-            output_path+"/out.gif",
-            save_all = True,
-            loop = abs(context.scene["loop_counts"]),
-            duration = abs(context.scene["duration"]),
-            transparency = 255,
-            disposal = 2,
-            append_images=image_list)
+        # save
+        if context.scene["gif_use_alpha"] == True:
+            first_img.save(
+                output_path+"/out.gif",
+                save_all = True,
+                loop = abs(context.scene["gif_loop_counts"]),
+                duration = abs(context.scene["gif_duration"]),
+                transparency = 255,
+                disposal = 2,
+                append_images=image_list)
+        else:
+            first_img.save(
+                output_path+"/out.gif",
+                save_all = True,
+                loop = abs(context.scene["gif_loop_counts"]),
+                duration = abs(context.scene["gif_duration"]),
+                disposal = 1,
+                append_images=image_list)
         return {"FINISHED"}
 
 
