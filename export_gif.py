7b5e3016f85724dfe61a111a85525bfd3b5db323
Suemura <39115013+Suemura@users.noreply.github.com>
2019-09-06 16:02:11 +0900

    fix list order

--- export_gif.py
+++ export_gif.py
@@ -21,6 +21,7 @@ class GIF_OT_ExportOperator(bpy.types.Operator):
         print("test : load_image_file")
         output_path = context.scene.render.filepath
         image_path_list = glob.glob(output_path+"/*.png", recursive=True)
+        image_path_list.sort()
         image_list = []
 
         # "use_alpha"
